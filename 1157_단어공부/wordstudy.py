525173dca1deb00eb2c1f0a11314e38f8505a52c
dori <sky980221@gmail.com>
2021-07-27 03:01:09 +0900

    backJoon 1157

--- 1157_단어공부/wordstudy.py
+++ 1157_단어공부/wordstudy.py
@@ -1,7 +1,14 @@
-a= input().upper
+a= input().upper()
 A_SET = list(set(a))
 
-new_list = []
-for i in a:
-  
-print(j)
+count_list = []
+
+for x in A_SET:
+    cnt = a.count(x)
+    count_list.append(cnt) #count 숫자를 리스트에 append 
+
+if count_list.count(max(count_list)) > 1: #count 숫자 최대값이 중복시 
+    print("?")
+else : 
+    max_index = count_list.index(max(count_list)) #count 숫자 최대값 위치
+    print(A_SET[max_index])
