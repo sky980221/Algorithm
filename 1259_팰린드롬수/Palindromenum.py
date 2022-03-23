7c3eb7460b9bfe3a2c04a2ff6a1d4ce2d9c410c7
Dori <sky980221@naver.com>
2022-03-23 17:51:03 +0900

    backJoon 1259

---
+++ 1259_팰린드롬수/Palindromenum.py
@@ -0,0 +1,18 @@
+
+while True:
+
+    n = input()
+    if n == '0':
+        break
+
+    if n == n[::-1]:
+        print("yes")
+            
+    else: 
+        print("no")
+
+'''if number == palin:
+    print("yes")
+else:
+    print("no")
+'''
