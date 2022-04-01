81ed5772fe3dad5f83430b8199628d044a7ae658
Dori <sky980221@naver.com>
2022-04-01 15:03:08 +0900

    backJoon 2869

---
+++ 2869_달팽이는 올라가고 싶다/snail.py
@@ -0,0 +1,19 @@
+A,B,V = list(map(int, input().split())) 
+
+#A는 낮에 올라가는 길이 2
+#B는 밤에 미끄러지는 길이 1 
+#V는 나무 막대의 길이 5
+
+H = A - B 
+
+Day = (V - A) // H 
+if A>= V:
+    Day = 1
+
+if (V-A) % H  ==0:
+    Day = Day + 1
+else: 
+    Day = Day + 2
+
+print(Day) 
+
