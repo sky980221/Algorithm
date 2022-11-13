f82581c792be231f8d68ced77214d1c9733640ca
Dori <sky980221@naver.com>
2022-11-13 23:49:34 +0900

    backJoon 2805

---
+++ 2805_나무 자르기/cuttingTree.py
@@ -0,0 +1,19 @@
+N, M = map(int, input().split())
+
+trees = list(map(int, input().split()))
+
+start , end = 1, sum(trees)
+
+while start <= end:
+    treesum = 0
+    mid = (start + end) // 2 
+    for i in trees:
+        if i > mid:
+            treesum += i - mid
+    if treesum >= M:
+        start = mid+1
+    else:
+        end = mid-1
+
+
+print(end)
