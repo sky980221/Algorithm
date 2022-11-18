1d61d292cc37c14b17845cc25e6e9d4bd4e19e53
Dori <sky980221@naver.com>
2022-11-18 20:52:29 +0900

    baekJoon 1149

---
+++ 1149_RGB거리/rgbLength.py
@@ -0,0 +1,14 @@
+N = int(input())
+color = []
+price = []
+
+for _ in range(N):
+    color.append(list(map(int, input().split())))
+
+for i in range(1, len(color)):
+    color[i][0] = min(color[i-1][1], color[i-1][2]) + color[i][0]
+    color[i][1] = min(color[i-1][0], color[i-1][2]) + color[i][1]
+    color[i][2] = min(color[i-1][0], color[i-1][1]) + color[i][2]
+
+print(min(color[N-1][0], color[N-1][1], color[N-1][2]))
+
