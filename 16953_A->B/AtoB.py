2940859557a826a55dca9a9999af96a7583bc477
Dori <sky980221@naver.com>
2023-01-16 14:39:47 +0900

    baekJoon 16953

--- 16953_A->B/AtoB.py
+++ 16953_A->B/AtoB.py
@@ -1,21 +1,22 @@
-a, b = map(int, input().split())
-result = 1
-while True:
+import sys
 
-    # a와 b가 같은 경우
+input = sys.stdin.readline
+a, b = map(int, input().split())
+
+count = 1
+while True:
     if a == b:
         break
-    elif (b % 2 != 0 and b % 10 != 1) or (b < a):
-        result = -1
+    elif a > b:
+        count = -1
         break
-
+    elif b % 10 == 1:
+        b //= 10
+        count += 1
+    elif b % 2 == 0:
+        b //= 2
+        count += 1
     else:
-
-        if b % 10 == 1:
-            b = b // 10
-            result += 1
-
-
-        else:
-            b = b // 2
-            result += 1
+        count = -1
+        break
+print(count)
