7f50e6c42b673e668314cd22b65fe7105b3adc2d
Dori <sky980221@naver.com>
2023-01-16 23:55:06 +0900

    baekJoon 2751

--- 2751_수정렬하기2/ascending_Power2.py
+++ 2751_수정렬하기2/ascending_Power2.py
@@ -1,32 +1,45 @@
 from heapq import merge
-from turtle import left
+import sys
 
-
-N = int(input())
+N = int(sys.stdin.readline())
 array = []
 for i in range(N):
-    array.append(int(input()))
-'''sort 함수를 사용하기에는 100만이라는 숫자가 너무 커서 merge sort를 사용해야함 nlogn의 속도를 가지고 있음 '''
+    array.append(int(sys.stdin.readline()))
 
-def mergeSort(array):
-    if len(array) <=1:
-        return array
-    mid = len(array)//2 
-    left = mergeSort(array[:mid])
-    right = mergeSort(array[mid:])
 
-    left1 = mergeSort(left)
-    right1 = mergeSort(right)
-    return = merge(left1, right1)
+# sort 함수를 사용하기에는 100만이라는 숫자가 너무 커서 merge sort를 사용해야함 nlogn의 속도를 가지고 있음
+
+def sort(arr):
+    if len(arr) < 2:
+        return arr
+
+    mid = len(arr) // 2
+    left = sort(arr[:mid])
+    right = sort(arr[mid:])
+
+    return merge(left, right)
+
 
 def merge(left, right):
-    i=j=0
-    sorted_list = []
-    
+    new_list = []
+    i = 0
+    j = 0
 
-while i < len(left) and j < len(mid):
+    while (i < len(left)) & (j < len(right)):
+        if left[i] > right[j]:
+            new_list.append(right[j])
+            j += 1
+        else:
+            new_list.append(left[i])
+            i += 1
+    while (j < len(right)):
+        new_list.append(right[j])
+        j += 1
+    while (i < len(left)):
+        new_list.append(left[i])
+        i += 1
+    return new_list
 
 
-
-for i in array:
+for i in sort(array):
     print(i)
