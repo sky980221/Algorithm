b71fb99bbea4cfa43c142172f45f2f1923ac64bf
Dori <sky980221@naver.com>
2023-01-18 18:52:09 +0900

    baekjoon 2805

--- 2805_나무 자르기/cuttingTree.py
+++ 2805_나무 자르기/cuttingTree.py
@@ -1,19 +1,20 @@
-N, M = map(int, input().split())
+import sys
 
-trees = list(map(int, input().split()))
+N, M = map(int, sys.stdin.readline().split())
 
-start , end = 1, sum(trees)
+trees = list(map(int, sys.stdin.readline().split()))
+
+start, end = 1, sum(trees)
 
 while start <= end:
     treesum = 0
-    mid = (start + end) // 2 
+    mid = (start + end) // 2
     for i in trees:
         if i > mid:
             treesum += i - mid
     if treesum >= M:
-        start = mid+1
+        start = mid + 1
     else:
-        end = mid-1
-
+        end = mid - 1
 
 print(end)
