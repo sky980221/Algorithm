73050a106873436e5e00b72dde56e2167f154f87
Dori <sky980221@naver.com>
2023-01-25 23:44:37 +0900

    baekJoon 1253

--- 1253_좋다/good.py
+++ 1253_좋다/good.py
@@ -1,31 +1,19 @@
-"""
-N개의 수 중에서 어떤 수가 다른 수 두 개의 합으로 나타낼 수 있다면 그 수를 “좋다(GOOD)”고 한다.
-
-N개의 수가 주어지면 그 중에서 좋은 수의 개수는 몇 개인지 출력하라.
-
-수의 위치가 다르면 값이 같아도 다른 수이다.
-
-"""
 import sys
-N = int(sys.stdin.readline())
-arr = list(map(int,sys.stdin.readline().split()))
 
-start = 0
-end = len(arr)
-cnt = 0 
+if __name__ == '__main__':
+    N = int(input())
+    arr = list(map(int, sys.stdin.readline().split()))
+    arr.sort()
+    ans = 0
 
-for i in arr:
-    tmp = arr[:i] + arr[i+2:]
-    start = 0
-    end = len(tmp)-1
-    while start < end:
-        mid = tmp[start] + tmp[end]
-        if arr[mid] == target:
-            cnt += 1 
-            break
-        elif arr[mid] > target:
-            end = mid -1
-        else:
-            start = mid +1
- 
-print(cnt)
+    for i in range(N):
+        tmp = arr[:i] + arr[i + 1:]
+        left, right = 0, len(tmp) - 1
+        while left < right:
+            t = tmp[left] + tmp[right]
+            if t == arr[i]:
+                ans += 1
+                break
+            if t < arr[i]: left += 1 # t 를 증가시켜야 하므로 left 증가
+            else: right -= 1 # t 를 감소시켜야 하므로 right 감소
+    print(ans)
