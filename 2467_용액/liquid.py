4c926463654bc38b103772e177c61341baed52e5
Dori <sky980221@naver.com>
2023-01-31 17:21:57 +0900

    baekJoon 2467

--- 2467_용액/liquid.py
+++ 2467_용액/liquid.py
@@ -1,18 +1,7 @@
 import sys
-import itertools
 from math import inf
 N = int(sys.stdin.readline())
 liquidArray = list(map(int, sys.stdin.readline().split()))
- 
-"""
-#두 개 용액 합쳐서 0에 가장 가깝게 만들어라 = 절댓값이 작아야함
-answerArray = list(itertools.combinations(liquidArray,2))
-newArray = []
-for i in range(len(answerArray)):
-    newArray.append(abs(sum(answerArray[i])))
-answer = newArray.index(min(newArray))
-print(*answerArray[answer])
-"""
 def binary_search(liquidArray):
     start = 0   
     end = N-1
