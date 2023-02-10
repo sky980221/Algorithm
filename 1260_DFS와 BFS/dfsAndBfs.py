4fddee448fbd5bdf3fd36a958a3f95b667482a9e
Dori <sky980221@naver.com>
2023-02-10 23:56:38 +0900

    baekJoon 1260

--- 1260_DFS와 BFS/dfsAndBfs.py
+++ 1260_DFS와 BFS/dfsAndBfs.py
@@ -1,49 +1,39 @@
 from collections import deque
+import sys
 
-def DFS(graph, root):
-    visited = []
-    stack = [root]
+N, M, V = map(int, sys.stdin.readline().split())
+# 정점 개수, 간선 개수, 시작 노드
+graph = [[False] * (N + 1) for _ in range(N + 1)]
 
-    while stack:
-        n = stack.pop()
-        if n not in visited:
-            visited.append(n)
-            if n in graph:
-                temp = list(set(graph[n]) - set(visited))
-                temp.sort(reverse=True)
-                stack += temp
-    return " ".join(str(i) for i in visited)
+for _ in range(M):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a][b] = True
+    graph[b][a] = True
 
-def BFS(graph, root):
-    visited = []
-    queue = deque([root])
+visited1 = [False] * (N + 1)  # dfs 방문기록
+visited2 = [False] * (N + 1)  # bfs 방문기록
 
-    while queue:
-        n = queue.popleft()
-        if n not in visited:
-            visited.append(n)
-            if n in graph:
-                temp = list(set(graph[n]) - set(visited))
-                temp.sort()
-                queue += temp
-    return " ".join(str(i) for i in visited)
 
-  
-graph = {}
-n = input().split(' ')
-node, edge, start = [int(i) for i in n]
-for i in range(edge):
-    edge_info = input().split(' ')
-    n1, n2 = [int(j) for j in edge_info]
-    if n1 not in graph:
-        graph[n1] = [n2]
-    elif n2 not in graph[n1]:
-        graph[n1].append(n2)
+def bfs(V):
+    queue = deque([V])
+    visited2[V] = True  # 해당 V값을 방문 처리
+    while queue:  # queue가 다 비면 종료
+        V = queue.popleft()  # queue 맨 밑에 있는 값을 꺼낸 뒤에 출력
+        print(V, end=" ")
+        for i in range(1, N + 1):
+            if not visited2[i] and graph[V][i]:
+                queue.append(i)
+                visited2[i] = True
 
-    if n2 not in graph:
-        graph[n2] = [n1]
-    elif n1 not in graph[n2]:
-        graph[n2].append(n1)
 
-print(DFS(graph, start))
-print(BFS(graph, start))
+def dfs(V):
+    visited1[V] = True  # 첫번째 노드 방문
+    print(V, end=" ")
+    for i in range(1, N + 1):
+        if not visited1[i] and graph[V][i]:
+            dfs(i)
+
+
+dfs(V)
+print()
+bfs(V)
