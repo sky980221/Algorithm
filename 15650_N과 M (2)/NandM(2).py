48083bf336a47a0b36933b41101aeb813d5c5a09
Dori <sky980221@naver.com>
2023-02-18 15:54:28 +0900

    baekJoon 15650

--- 15650_N과 M (2)/NandM(2).py
+++ 15650_N과 M (2)/NandM(2).py
@@ -9,3 +9,24 @@ arr = [i + 1 for i in range(N)]
 answer = list(combinations(arr, M))
 for i in range(len(answer)):
     print(*answer[i])
+
+"""
+def dfs(num):
+    if len(result) == m:
+        print(" ".join(map(str, result)))
+        return
+    else:
+        for i in range(num, n + 1):
+            if not visited[i]:
+                visited[i] = 1
+                result.append(i)
+                dfs(i + 1)
+                visited[i] = 0
+                result.pop()
+
+
+n, m = map(int, input().split())
+visited = [0] * (n + 1)
+result = []
+dfs(1)  
+"""
