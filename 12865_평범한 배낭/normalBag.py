444fd871ca1fd3201133ecff03eb61dc12327ab6
Dori <sky980221@naver.com>
2023-02-21 23:28:09 +0900

    baekJoon 12865

--- 12865_평범한 배낭/normalBag.py
+++ 12865_평범한 배낭/normalBag.py
@@ -1,19 +1,37 @@
 import sys
 
+
 n, k = map(int, sys.stdin.readline().split())
-arr = [(0, 0)]
-chart = [[0] * (k + 1) for _ in range(n + 1)]
-for _ in range(n):
-    w, v = map(int, sys.stdin.readline().split())
-    arr.append((w, v))
 
-for i in range(1, n + 1):
-    for j in range(1, k + 1):
-        w = arr[i][0]
-        v = arr[i][1]
+thing = [[0,0]]
+d = [[0]*(k+1) for _ in range(n+1)]
+
+for i in range(n):
+    thing.append(list(map(int, sys.stdin.readline().split())))
+for i in range(1, n+1):
+    for j in range(1, k+1):
+        w = thing[i][0]
+        v = thing[i][1]
+
         if j < w:
-            chart[i][j] = chart[i - 1][j]
+            d[i][j] = d[i-1][j]
         else:
-            chart[i][j] = max(chart[i - 1][j], v + chart[i - 1][j - w])
+            d[i][j] = max(d[i-1][j], d[i-1][j-w]+v)
 
-print(chart[n][k])
+print(d[n][k])
+
+"""
+4 7
+6 13
+4 8
+3 6
+5 12
+
+다른 테케 찾아보장 
+
+4 9 
+3 11
+9 12 
+4 8
+2 5  
+"""
