3fb0a52fe17ee609d51128b2e184fb9885333964
Dori <sky980221@naver.com>
2023-03-10 16:51:59 +0900

    baekJoon 2579

--- 2579_계단 오르기/stair.py
+++ 2579_계단 오르기/stair.py
@@ -7,13 +7,13 @@ for _ in range(N):
     arr.append(int(sys.stdin.readline()))
 
 if len(arr) <= 2:
-    print(arr)
+    print(sum(arr))
 else:
     dp[0] = arr[0]
     dp[1] = arr[0] + arr[1]
 
     for i in range(2, N):
-        dp[i] = max(dp[i - 3] + arr[i - 1], dp[i - 2] + arr[i])
+        dp[i] = max(dp[i - 3] + arr[i - 1]+ arr[i], dp[i - 2] + arr[i])
     print(dp[-1])
 """
 
