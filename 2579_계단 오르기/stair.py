4a0786d7b569e52fbbd2dca71349c2b470a5cdd3
Dori <sky980221@naver.com>
2023-03-17 23:37:17 +0900

    baekJoon 2579

--- 2579_계단 오르기/stair.py
+++ 2579_계단 오르기/stair.py
@@ -13,7 +13,7 @@ else:
     dp[1] = arr[0] + arr[1]
 
     for i in range(2, N):
-        dp[i] = max(dp[i - 3] + arr[i - 1]+ arr[i], dp[i - 2] + arr[i])
+        dp[i] = max(dp[i - 3] + arr[i - 1] + arr[i], dp[i - 2] + arr[i])
     print(dp[-1])
 """
 
