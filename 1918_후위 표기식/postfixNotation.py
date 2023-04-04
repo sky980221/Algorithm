89b3222169ae49b1cb0bce9ec57256b58f533c86
Dori <sky980221@naver.com>
2023-04-04 17:31:46 +0900

    baekJoon 1918

---
+++ 1918_후위 표기식/postfixNotation.py
@@ -0,0 +1,25 @@
+import sys
+arr = list(sys.stdin.readline())
+stack = []
+res = ''
+for s in arr:
+    if s.isalpha():
+        res += s
+    else:
+        if s == '(':
+            stack.append(s)
+        elif s == '*' or s == '/':
+            while stack and (stack[-1] == '*' or stack[-1] == '/'):
+                res += stack.pop()
+            stack.append(s)
+        elif s == '+' or s == '-':
+            while stack and stack[-1] != '(':
+                res += stack.pop()
+            stack.append(s)
+        elif s == ')':
+            while stack and stack[-1] != '(':
+                res += stack.pop()
+            stack.pop()
+while stack:
+    res += stack.pop()
+print(res)
