ea1754501ad3d19b037b791bbafbfa9856525fbb
Dori <sky980221@naver.com>
2023-05-08 22:04:40 +0900

    baekJoon 18870

--- 18870_좌표 압축/compressingCoordinates.py
+++ 18870_좌표 압축/compressingCoordinates.py
@@ -9,10 +9,3 @@ arr2 = sorted(list(set(arr)))
 dic = {arr2[i] : i for i in range(len(arr2))}
 for i in arr:
     print(dic[i], end = ' ')
-"""
-Test Case1
-5
-2 4 -10 4 -9
-출력 
-2 3 0 3 1
-"""                                                                         
