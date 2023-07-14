00a0529a788c9028595bf0a49da2ef01f5b60e64
Dori <sky980221@naver.com>
2023-07-14 23:35:41 +0900

    baekJoon 17219

---
+++ 17219_비밀번호 찾기/findPassword.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+MEMO = {}
+
+for n in range(N):
+    key, value = input().split()
+    MEMO[key] = value
+
+for m in range(M):
+    key = input().strip()
+    print(MEMO[key])
+
