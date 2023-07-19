7ae3c2fa2c4e546d01e91481a5edde63cc6e3aab
Dori <sky980221@naver.com>
2023-07-19 23:55:55 +0900

    baekJoon 6064

---
+++ 6064_카잉 달력/Inka.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+T = int(input())
+
+
+def find_K(M, N, x, y):
+    K = x
+
+    while K <= M * N:
+        if (K - x) % M == 0 and (K - y) % N == 0:
+            return K
+        K += M
+    return -1
+
+
+for _ in range(T):
+    M, N, x, y = map(int, input().rstrip().split())
+    print(find_K(M, N, x, y))
