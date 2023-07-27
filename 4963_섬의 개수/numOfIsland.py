6a08912925ea4bb80078bcd6873140ccbb7e483a
Dori <sky980221@naver.com>
2023-07-27 23:45:48 +0900

    baekJoon 4963

--- 4963_섬의 개수/numOfIsland.py
+++ 4963_섬의 개수/numOfIsland.py
@@ -3,9 +3,9 @@ from collections import deque
 
 input = stdin.readline
 
+"""
 dx = [1, -1, 0, 0, 1, -1, 1, -1]
 dy = [0, 0, 1, -1, 1, -1, -1, 1]
-
 
 def bfs(x, y):
     que = deque()
@@ -38,3 +38,50 @@ while True:
 
 for i in result:
     print(i)
+    
+    
+"""
+# 2회차 풀이 20230727
+
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+d = [[1, 0], [0, 1], [-1, 0], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]]
+
+
+def bfs(y, x):
+    q = deque()
+    q.append([y, x])
+    visited[y][x] = True
+
+    while q:
+        r, c = q.popleft()
+
+        for i in range(8):
+            dr = r + d[i][0]
+            dc = c + d[i][1]
+
+            if 0 <= dr < h and 0 <= dc < w and maps[dr][dc] == 1 and not visited[dr][dc]:
+                visited[dr][dc] = True
+                q.append([dr, dc])
+
+
+while True:
+    w, h = map(int, input().split())
+
+    if w == 0 and h == 0:
+        break
+
+    maps = [list(map(int, input().split())) for _ in range(h)]
+    visited = [[False] * w for _ in range(h)]
+
+    count = 0
+    for i in range(h):
+        for j in range(w):
+            if maps[i][j] == 1 and not visited[i][j]:
+                bfs(i, j)
+                count += 1
+
+    print(count)
