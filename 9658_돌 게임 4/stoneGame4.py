fc78771a91723c5e84aeea63bc5ba03b0dbc2a48
Dori <sky980221@naver.com>
2023-08-04 23:56:56 +0900

    baekJoon 9658

---
+++ 9658_돌 게임 4/stoneGame4.py
@@ -0,0 +1,25 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+
+def solution(n):
+    dp = [0] * (n + 5)
+    dp[1], dp[2], dp[3], dp[4] = 0, 1, 0, 1
+
+    for i in range(5, n + 1):
+        if dp[i - 1] and dp[i - 3] and dp[i - 4]:
+            dp[i] = 0
+        else:
+            dp[i] = 1
+
+    if dp[n] == 1:
+        print('SK')
+    else:
+        print('CY')
+    return dp[-1]
+
+
+solution(N)
