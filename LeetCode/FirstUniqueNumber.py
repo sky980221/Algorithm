ad6558c05e8942e41a9e2bb6d92e55497810260d
Dori <sky980221@naver.com>
2023-09-03 16:56:25 +0900

    LeetCode_FirstUniqueNumber

---
+++ LeetCode/FirstUniqueNumber.py
@@ -0,0 +1,15 @@
+#1429
+
+class FirstUnique:
+    def __init__(self, nums: List[int]):
+        self.cnt = Counter(nums)
+        self.q = deque(nums)
+
+    def showFirstUnique(self) -> int:
+        while self.q and self.cnt[self.q[0]] != 1:
+            self.q.popleft()
+        return -1 if not self.q else self.q[0]
+
+    def add(self, value: int) -> None:
+        self.cnt[value] += 1
+        self.q.append(value)
