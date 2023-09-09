5d5baf87dd88930cbe54db29df86f34f28711c5e
Dori <sky980221@naver.com>
2023-09-09 17:30:57 +0900

    LeetCode_BestTimeToBuyAndSellStockII

---
+++ LeetCode/BestTimeToBuyAndSellStockII.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) < 2:
+            return 0
+        if len(prices) == 2:
+            output = prices[1] - prices[0]
+            return output if output > 0 else 0
+        i = 0
+        j = 1
+        stockBuy = prices[i]
+        stockSell = prices[j]
+        counter = 0
+        profit = 0
+        while counter < len(prices)-2:
+            if stockSell - stockBuy < 0:
+                i = counter + 1
+                j = i + 1
+            else:
+                if prices[j+1] > prices[j]:
+                    j += 1
+                else:
+                    profit = profit + (stockSell - stockBuy)
+                    i = counter + 1
+                    j = i + 1
+
+            stockSell = prices[j]
+            stockBuy = prices[i]
+            counter += 1
+        if (stockSell - stockBuy) > 0:
+            profit = profit + (stockSell - stockBuy)
+        return profit
