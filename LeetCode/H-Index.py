d8f8a3a5a7b6931f5fbddef746e1e4c91cb6a57a
Dori <sky980221@naver.com>
2023-10-02 23:57:00 +0900

    LeetCode_H-Index

---
+++ LeetCode/H-Index.py
@@ -0,0 +1,15 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort()
+        n = len(citations)
+        h = 0
+
+        if n == 1 and citations[0] == 0:
+            return 0
+        else:
+            for i in range(n):
+                if citations[i] < n - i:
+                   h = citations[i]
+                else:
+                    h = max(h, n-i)
+            return h
