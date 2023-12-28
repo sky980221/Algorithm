14a372aa60bc6b933b9ee444b1889ccd9a341735
Dori <sky980221@naver.com>
2023-12-28 23:14:36 +0900

    baekJoon 10814

--- 10814_나이순 정렬/ageorder.py
+++ 10814_나이순 정렬/ageorder.py
@@ -1,11 +1,15 @@
-N = int(input())
-array = [[0 for col in range(2)] for row in range(100000)]
-for _ in range(N):
-    age, name = input().split
-    #입력 받았으니까 나이하고 이름 배열에 넣어줌
-    array.append(age,name)
+N = int(input())  # 가입한 사람의 수 입력
+members = []
 
-print(array)
+for _ in range(N):
+    age, name = input().split()
+    members.append((int(age), name))  # 나이를 정수로 변환하여 저장
+
+# 나이를 기준으로 정렬 (나이가 같으면 가입 순서 유지)
+members.sort(key=lambda x: x[0])
+
+for member in members:
+    print(member[0], member[1])
 
 '''
 1. 이름과 age를 N번 만큼 입력 받은 후  배열에 넣는다 
