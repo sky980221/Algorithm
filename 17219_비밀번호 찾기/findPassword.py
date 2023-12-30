b9e12ab57248bc11e2a3145bce93f8ab43d9c7ee
Dori <sky980221@naver.com>
2023-12-30 23:36:44 +0900

    baekJoon 17219

--- 17219_비밀번호 찾기/findPassword.py
+++ 17219_비밀번호 찾기/findPassword.py
@@ -1,14 +1,34 @@
+# 20231230 다시 풀기
+import sys
+
+# 입력 받기
+N, M = map(int, input().split())
+Arr1 = {}
+for _ in range(N):
+    a, b = input().split()
+    Arr1[a] = b
+
+Arr2 = [input().strip() for _ in range(M)]
+
+# 결과 출력
+for word in Arr2:
+    if word in Arr1:
+        print(Arr1[word])
+
+
+"""
+
 import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-MEMO = {}
+add = {}
 
-for n in range(N):
-    key, value = input().split()
-    MEMO[key] = value
+for _ in range(N):
+    site, ps = input().split()
+    add[site] = ps
 
-for m in range(M):
-    key = input().strip()
-    print(MEMO[key])
-
+for _ in range(M):
+    print(add[input().rstrip()])
+    
+"""
