801bff0b87ff085d5db609f7933b41d4c4eed213
Dori <sky980221@naver.com>
2023-12-31 17:12:11 +0900

    baekJoon 10816

--- 10816_숫자 카드2 /numCard.py
+++ 10816_숫자 카드2 /numCard.py
@@ -1,18 +1,51 @@
+"""import sys
+Arr1 = []
+Arr2 = []
+N = int(input())
+# Arr1 = input().split()
+Arr1.extend(list(map(int, input().split())))  # 입력 값을 리스트로 저장
+M = int(input())
+Arr2.extend(list(map(int, input().split())))  # 입력 값을 리스트로 저장
+"""
+
+"""
 import sys
 
+cnt = 0
+# 쉽게 가자 쉽게
 N = int(input())
-arr1 = sorted(list(map(int, input())))
+Arr1 = [int(x) for x in input().split()]  # 입력 값을 리스트로 저장
 M = int(input())
-arr2 = sorted(list(map(int, input())))
+Arr2 = [int(x) for x in input().split()]  # 입력 값을 리스트로 저장
 
-cnt = {}
-for i in arr1:
-    if i in cnt:
-        cnt[i] += 1
+for num in Arr2:
+    for num2 in Arr1:
+        if num == num2:
+            cnt += 1
+
+    print(cnt, end=" ")
+    cnt = 0
+"""
+
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+cards = [*map(int, input().split())]
+M = int(input())
+candidate = [*map(int, input().split())]
+
+count = {}
+for card in cards:
+    if card in count:
+        count[card] += 1
     else:
-        cnt[i] = 1
-for i in arr2:
-    if i in cnt:
-        print(cnt[i], end='')
+        count[card] = 1
+
+for target in candidate:
+    result = count.get(target)
+    if result is None:
+        print(0, end=" ")
     else:
-        print(0, end='')
+        print(result, end=" ")
