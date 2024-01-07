2dac3d184e79c527b35a1daa48deb7ff3a66b603
Dori <sky980221@naver.com>
2024-01-07 20:01:15 +0900

    baekJoon 1620

--- 1620_나는야 포켓몬 마스터 이다솜/pocketmonMaster.py
+++ 1620_나는야 포켓몬 마스터 이다솜/pocketmonMaster.py
@@ -1,20 +1,19 @@
-from sys import stdin
+import sys
 
-def input():
-    return stdin.readline().rstrip()
+input = sys.stdin.readline
 
+N, M = map(int, input().split())
 
-n, m = map(int, input().split())
-findId = {}
-findName = {}
-for i in range(1, n + 1):
-    pokemon = input()
-    findId[i] = pokemon
-    findName[pokemon] = i
+pokedex = {}  # 빈 딕셔너리 생성
 
-for _ in range(m):
-    x = input()
-    if x.isdigit():
-        print(findId[int(x)])
-    else:
-        print(findName[x])
+for i in range(1, N + 1):
+    pokemon_name = input().strip()
+    pokedex[i] = pokemon_name
+    pokedex[pokemon_name] = i
+print(pokedex)
+for j in range(M):
+    find = input().strip()
+    if find.isdigit():  # 입력이 숫자인 경우
+        print(pokedex[int(find)])
+    else:  # 입력이 문자열인 경우
+        print(pokedex[find])
