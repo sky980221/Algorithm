1ac90bdbe4428741f714a858a3563a002a4c501c
Dori <sky980221@naver.com>
2024-01-08 23:17:52 +0900

    baekJoon 11726

--- 11726_2xn 타일링/2xntiling.py
+++ 11726_2xn 타일링/2xntiling.py
@@ -1,3 +1,4 @@
+"""
 n = int(input())
 
 dp = [0]*(n+1)
@@ -14,3 +15,17 @@ else:
         dp[i] = dp[i-1]+dy[i-2]
 
     print(dp[n]%10007)
+
+    """
+
+n = int(input())
+
+#1001이 아니라 n+1일 경우엔 dp[2]=2를 넣을 공간이 없음.
+dp = [0]*1001
+dp[1] = 1
+dp[2] = 2
+
+for i in range(2, n):
+    dp[i+1] = dp[i] + dp[i-1]
+
+print(dp[n] % 10007)
