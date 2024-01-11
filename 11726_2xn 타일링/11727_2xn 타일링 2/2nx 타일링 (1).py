78330320ffe97f9e565ad41ddd20a356db93a9cd
Dori <sky980221@naver.com>
2024-01-11 23:58:41 +0900

    baekJoon 11726

--- 11726_2xn 타일링/11727_2xn 타일링 2/2nx 타일링 (1).py
+++ 11726_2xn 타일링/11727_2xn 타일링 2/2nx 타일링 (1).py
@@ -1,4 +1,4 @@
-import sys
+"""import sys
 input = sys.stdin.readline
 
 n = int(input())
@@ -13,3 +13,18 @@ for i in range(2, n+1):
     dp[i] = dp[i-1] + 2 * dp[i-2]
 
 print(dp[n]%10007)
+"""
+
+#20240111
+
+n = int(input())
+
+dp = [0]*1001
+dp[1] = 1
+dp[2] = 3
+
+for i in range(3,n+1):
+    dp[i] = dp[i-1]+dp[i-2]*2
+
+print(dp[n]%10007)
+
