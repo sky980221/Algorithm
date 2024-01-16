99b44f4f52ebef1b43e90a8bc0b92a02f2bb46dd
Dori <sky980221@naver.com>
2024-01-16 23:59:10 +0900

    baekJoon 2178

--- 2178_미로 탐색/miroSearch.py
+++ 2178_미로 탐색/miroSearch.py
@@ -1,34 +1,26 @@
 import sys
 from collections import deque
 
-input = sys.stdin.readline
+n, m = map(int, sys.stdin.readline().split())
+array = []
 
-n, m = map(int, input().split())
-graph = []
+for i in range(n):
+    array.append(list(map(int, sys.stdin.readline().strip())))
 
-for _ in range(n):
-    graph.append(list(map(int, input().rstrip()))) # readline의 경우 맨 뒤에 '\n'까지 입력받으므로 제거해줘야 함
+d = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 
-# 상하좌우
-dx = [-1, 1, 0, 0] 
-dy = [0, 0, -1, 1]
 
-def bfs(x, y):
-    
+def bfs(y, x):
     queue = deque()
-    queue.append((x,y))
-
+    queue.append((y, x))
     while queue:
-        x, y = queue.popleft()
+        y1, x1 = queue.popleft()
+        for dx, dy in d:
+            if 0 <= y1 + dy < n and 0 <= x1 + dx < m:
+                if array[y1 + dy][x1 + dx] == 1:
+                    array[y1 + dy][x1 + dx] = array[y1][x1] + 1
+                    queue.append((y1 + dy, x1 + dx))
 
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
 
-            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 1:
-                queue.append((nx, ny))
-                graph[nx][ny] = graph[x][y] + 1
-    
-    return graph[n-1][m-1]
-
-print(bfs(0,0))
+bfs(0, 0)
+print(array[n - 1][m - 1])
