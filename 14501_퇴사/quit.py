6c7789dc8023faa61c17664a02db37cdef261d65
Dori <sky980221@naver.com>
2024-01-19 23:45:48 +0900

    baekJoon 14501

---
+++ 14501_퇴사/quit.py
@@ -0,0 +1,17 @@
+n = int(input())
+T, P = [0 for i in range(n+1)], [0 for i in range(n+1)]
+for i in range(n):
+    a,b = map(int, input().split())
+    T[i] = a
+    P[i] = b
+
+# dp[i]는 i번째날까지 일을 했을 때, 최대값이다. 
+dp =[0 for i in range(n+1)]
+
+for i in range(len(T)-2, -1, -1):      # 역순으로 진행
+    if T[i]+i <= n:       # 날짜를 초과하지 않을 경우.
+        dp[i] = max(P[i] + dp[i + T[i]], dp[i+1])   
+    else:                 # 날짜를 초과할 경우.
+        dp[i] = dp[i+1]
+print(dp[0])
+출처: https://jrc-park.tistory.com/119 [🐾 Rudi Park:티스토리]
