b3b4d225c2c947c003e170ec61da90b57b27926f
Dori <sky980221@naver.com>
2024-01-22 23:38:59 +0900

    baekJoon 1904

---
+++ 1904_01타일/01tile.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+dp = [0] * 1000001
+dp[1] = 1
+dp[2] = 2
+
+for k in range(3, n + 1):
+    dp[k] = (dp[k - 1] + dp[k - 2]) % 15746
+print(dp[n])
+출처: https: // sungmin - joo.tistory.com / 21[JooTopia
