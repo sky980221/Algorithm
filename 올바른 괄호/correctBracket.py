51fe9425764350e2964c14b3b56570a534257d1c
Dori <sky980221@naver.com>
2024-02-04 00:06:22 +0900

    PROGRAMMERS - 올바른 괄호

--- 올바른 괄호/correctBracket.py
+++ 올바른 괄호/correctBracket.py
@@ -1,13 +1,16 @@
 def solution(s):
     stack = []
     for c in s:
+        #C가 열린 괄호라면 스택에 저장함.
         if c == "(":
             stack.append(c)
+        #닫힌 괄호라면, 스택이이 차 있는지 확인하고 차 있다면 맨 뒤에 것을 제거, 비어있다면 False 출력
         else:
             if stack:
                 stack.pop()
             else:
                 return False
+        #일련의 과정을 끝낸 뒤 스택이 비어있지 않다면 False 출력, 비어있어야 True.
     if stack:
         return False
     return True
